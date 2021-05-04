11973994f757ab550b02d7b15760272a69cc82ac
Aditya-Prabhu <52061073+Aditya-Prabhu@users.noreply.github.com>
2021-05-04 14:32:44 +0530

    bot created

---
+++ inkBot.py
@@ -0,0 +1,165 @@
+#importing the discord package
+import discord
+from discord.ext import commands
+import random
+import inkFunCommands
+import time
+import inkServerManageCommands
+import token
+
+# Client (my bot)
+client = commands.Bot(command_prefix = ['ink ', 'ink'])
+client.remove_command('help')
+
+
+#doing stuff
+
+#vars for the bot
+botVars_describeList = inkFunCommands.describeList
+botVars_pickupLines = inkFunCommands.pickupLines
+
+#message sent in bot status when bot comes on
+@client.event
+async def on_ready():
+    item_channel = client.get_channel(838425596421079060)
+    await item_channel.send('bot is up! <:yayy:838430088738504725>')
+
+#message sent in bot status when bot goes down
+@client.event
+async def on_disconnect():
+    item_channel = client.get_channel(838425596421079060)
+    await item_channel.send('bot is now down <:sad:838430133537734737>')
+
+#help command
+@client.group(name="help",invoke_without_command=True)
+async def help_cmd(context):
+    helpEmbed = discord.Embed(Title="inkBot help",description='Say `ink help <command>` for more info about a particular command',colour=0x9933ff)
+    helpEmbed.add_field(name='Fun',value='`say`,`describe`,`hi`,`help`,`iq`,`besmooth`',inline=False)
+    helpEmbed.add_field(name='Server Utils',value='`whois`,`info`',inline=False)
+    helpEmbed.add_field(name='Server Config',value='`autobanScammers`',inline=False)
+    helpEmbed.set_footer(text='bot by Adi#1874')
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def say(context):
+    helpEmbed = discord.Embed(title='Say command',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Makes the bot repeat something for you',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`ink say <your_text>`',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def describe(context):
+    helpEmbed = discord.Embed(title='Describe command',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Makes the bot describe the person you ping',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`ink describe <target>`',inline=False)
+    helpEmbed.add_field(name='Xtra Feature:', value='type `ink describe list` to see list of possible descriptions',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def hi(context):
+    helpEmbed = discord.Embed(title='hi ink command',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Makes the bot reply with a hi',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`hi ink`',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def help(context):
+    helpEmbed = discord.Embed(title='Help command',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Opens the `ink help` embed to see all ink features and find out how to use them',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`ink help`',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def iq(context):
+    helpEmbed = discord.Embed(title='Iq command',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Generates a random value to be the iq for the user mentioned',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`ink iq <target>`',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def besmooth(context):
+    helpEmbed = discord.Embed(title='besmooth command',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Generates a random pickup line',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`ink besmooth`',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def whois(context):
+    helpEmbed = discord.Embed(title='Gives info on a mentioned user',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Gives info on a user regardless of wether they are in the server(Use `ink info` for more detailed info on a user in the server)',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`ink whois <user>`',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def info(context):
+    helpEmbed = discord.Embed(title='Gives info on a mentioned user',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Gives detailed info on a user in the server(Use `ink whois` for info on a user regardless of wether they are in the server )',inline=False)
+    helpEmbed.add_field(name='Syntax',value='`ink info <user>`',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+@help_cmd.command()
+async def autobanScammers(context):
+    helpEmbed = discord.Embed(title='Autoban scammers',colour=0x9933ff)
+    helpEmbed.add_field(name='Feature', value='Automatically bans members that are reported in the alert channels of various servers',inline=False)
+    helpEmbed.add_field(name='Setup',value='You have to dm Adi for this till he learns to set up global vars',inline=False)
+    await context.message.channel.send(embed=helpEmbed)
+
+
+#command to echo
+@client.command(name = 'say')
+async def say(context, *, whatToSay):
+    await inkFunCommands.say(context,whatToSay)
+
+#gives a description of the person mentioned
+@client.command(name = 'describe')
+async def describe(context):
+    await inkFunCommands.describe(context)
+
+#iq command
+@client.command(name = 'iq')
+async def iq(context):
+    await inkFunCommands.iq(context)
+
+#flirt command
+@client.command(name = 'besmooth')
+async def besmooth(context):
+    await inkFunCommands.besmooth(context)
+
+#whois feature
+@client.command(name = 'whois')
+async def whois(context, target : discord.User):
+    await inkServerManageCommands.whois(context,target)
+
+#info feature
+@client.command(name = 'info')
+async def info(context, target : discord.Member):
+    print('info command called')
+    await inkServerManageCommands.info(context,target)
+
+#triggers which include: 
+#1. hi ink trigger
+#adi ar
+#Scammer Autoban
+#NQN features(WIP)
+@client.event
+async def on_message(message):
+    if 'hi ink' == message.content.lower():
+        if str(message.author.nick) != 'None':
+            await message.channel.send('Hi ' + str(message.author.nick))
+        else:
+            await message.channel.send('Hi ' + str(message.author.name))
+        
+    elif 'adi' in message.content.lower():
+        await message.add_reaction('<:hmm:839059852554666024>')
+
+    elif message.channel.id == 838693642209853441:
+        if 'id: ' == message.content.lower()[0:4]:
+            userToBan = await client.fetch_user(int(message.content[4:23]))
+            await message.channel.send(f"""Banning user:{userToBan.name}(#{userToBan.discriminator}) with id:{userToBan.id} for scam reported in the message {message.jump_url}""")
+            await message.guild.ban(user= userToBan,reason= 'Ink autoban for being reported by another server. [Link to alert](' + message.jump_url + ')')
+
+
+    await client.process_commands(message)
+
+#Running client on server
+client.run('ODM5MDUyNTM5Mzk1NDQwNzEw.YJECkg._ZFMBhlOzRncu73SCBujxV4wFQY')
---
+++ inkFunCommands.py
@@ -0,0 +1,57 @@
+#importing required stuff
+import random
+import discord
+
+#base vars for the commands
+describeList = ['big nab','a qt','simp','hot','friendly','an irritating bitch','an ass','a dick','a hoe','dum','tiny','the best','the poggest','is sweet','is cute','<:bigbrain:838472543705759824>','teeny brain','an alien','a waifu','a dumbass']
+pickupLines = ['I hope you know CPR, because you just took my breath away!','So, aside from taking my breath away, what do you do for a living?','I ought to complain to Spotify for you not being named this week’s hottest single.','Are you a parking ticket? ‘Cause you’ve got ‘fine’ written all over you.','Your eyes are like the ocean; I could swim in them all day.','When I look in your eyes, I see a very kind soul.','If you were a vegetable, you’d be a ‘cute-cumber.’','Do you happen to have a Band-Aid? ‘Cause I scraped my knees falling for you.','I never believed in love at first sight, but that was before I saw you.',' I didn’t know what I wanted in a woman until I saw you.','I was wondering if you could tell me: If you’re here, who’s running Heaven?','(at night) No wonder the sky is dark all the color is in your eyes.','You’ve got everything I’ve been searching for, and believe me—I’ve been looking a long time.','You’re like a fine wine. The more of you I drink in, the better I feel.','You’ve got a lot of beautiful curves, but your smile is absolutely my favorite.','Are you as beautiful on the inside as you are on the outside?','If being sexy was a crime, you’d be guilty as charged.','I was wondering if you’re an artist because you were so good at drawing me in.','It says in the Bible to only think about what’s pure and lovely… So I’ve been thinking about you all day long.','Do you have a map? I just got lost in your eyes.','I’d like to take you to the movies, but they don’t let you bring in your own snacks.','You know what you would look really beautiful in? My arms.','I would never play hide and seek with you because someone like you is impossible to find.','Are you a magician? It’s the strangest thing, but every time I look at you, everyone else disappears.','I think there’s something wrong with my phone. Could you try calling it to see if it works?','Hi, I just wanted to thank you for the gift. (pause) I’ve been wearing this smile ever since you gave it to me.','Are you an electrician? Because you’re definitely lighting up my day/night!','I’ve heard it said that kissing is the ‘language of love.’ Would you care to have a conversation with me about it sometime?','I always thought happiness started with an ‘h,’ but it turns out mine starts with ‘u.’','I believe in following my dreams. Can I have your Instagram?','Do you know what the Little Mermaid and I have in common? We both want to be part of your world.','If you were a song, you’d be the best track on the album.','You know, I always thought that Disneyland was the ‘happiest place on Earth,’ but that was before I got a chance to stand here next to you.','Want to go outside and get some fresh air with me? You just took my breath away.','If you were a taser, you’d be set to ‘stun.’','If you were a Transformer, you’d be ‘Optimus Fine.’','Is your name Google? Because you have everything I’m searching for.','Do you ever get tired from running through my thoughts all night?','You know, they say that love is when you don’t want to sleep because reality is better than your dreams. And after seeing you, I don’t think I ever want to sleep again.','Trust me, I’m not drunk; I’m just intoxicated by you.','Do you have a name, or can I just call you ‘mine?’','if u were a fruit, you\'d be a fineapple','Are you a tazer? cause you\'re stunning']
+
+#describe command code
+async def describe(context):
+    try:
+        personToDescribe = context.message.mentions[0].id
+        peopleDescriptions = describeList
+        ranvar = (random.choice(peopleDescriptions))
+        if context.message.mentions[0].id == 652756616185380894:  
+            await context.message.channel.send('<@' + str(personToDescribe) + '> is awesome')
+        else:
+            await context.message.channel.send('<@' + str(personToDescribe) + '> is ' + ranvar)
+    except IndexError:
+        if context.message.content[13:18] == 'list':
+            displayList = ', '.join(describeList)
+            await context.message.channel.send(displayList)
+        else:
+            await context.message.channel.send('<@' + str(context.message.author.id) + '> mention someone to describe, dumbass <:facepalm:838671083481333781>')
+        
+    except:
+        await context.message.channel.send('something went wrong and idfk wat')
+
+#say command code
+async def say(context,whatToSay):
+    await context.message.channel.send(whatToSay)
+
+#iq command code
+async def iq(context):
+    try:
+        if context.message.mentions[0].id == 652756616185380894:
+            personIq = 160
+        else:
+            personIq = random.randint(a= 40, b= 160)
+        
+        if personIq >= 116:
+            personEmoji = '<:bigbrain:838472543705759824>'
+        elif personIq >= 84:
+            personEmoji = ':brain:'
+        elif personIq >= 40:
+            personEmoji = '<:dumbfuck:838730636175998976>'
+        
+        iqEmbed = discord.Embed(title=f"""{context.message.mentions[0].name}'s iq:""",description=f"""<@{context.message.mentions[0].id}> has an iq of {str(personIq)} {personEmoji}""",color= 0xabcdef)
+        await context.message.channel.send(embed= iqEmbed)
+    except IndexError:
+        await context.message.channel.send('Oi nub you forgot to mention someone! <a:PI_Angry:838736380674572328>')
+    except:
+        await context.message.channel.send('Fuck! something went wrong and idk what ;-;')
+
+#flirt command  
+async def besmooth(context):
+    await context.message.channel.send(random.choice(pickupLines))
