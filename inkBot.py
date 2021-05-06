cb79fd0eef83654db580f9ae9c1c1f57d319114f
Aditya-Prabhu <52061073+Aditya-Prabhu@users.noreply.github.com>
2021-05-06 19:33:18 +0530

    Reworked autoban release

--- inkBot.py
+++ inkBot.py
@@ -209,8 +209,8 @@ async def on_message(message):
                 await message.add_reaction('<:an_urcute:776896089760333844>')
             
 
-    elif message.channel.id in varsToNotCopy.alertChannel_id:
-        if 'id' == message.content.lower()[0:2]:
+    elif 'id' == message.content.lower()[0:2]:
+        if message.channel.id in varsToNotCopy.alertChannel_id:
             try:
                 userToBan = int(message.content[4:22])
                 userToBan = str(userToBan)
@@ -231,7 +231,26 @@ async def on_message(message):
                 await message.channel.send('I detected something.... but its not an id....')
             except:
                 await message.channel.send('Couldnt find ID, ping adi if this happens too often and he\'ll prolly think of a fix <a:MaidThumbsUp:839411010460975165>')
-    
+        
+        elif message.channel.id in varsToNotCopy.failChannel_id:
+            try:
+                userToBan = int(message.content[4:22])
+                userToBan = str(userToBan)
+                if len(userToBan) == 18:
+                    await message.delete()
+                else:
+                    userToBan = int(message.content[3:21])
+                    userToBan = str(userToBan)
+                    if len(userToBan) == 18:
+                        await message.delete()
+                    else:
+                        await message.channel.send('Couldnt find ID, ping adi if this happens too often and he\'ll prolly think of a fix <a:MaidThumbsUp:839411010460975165>')
+            except ValueError:
+                await message.channel.send('I detected something.... but its not an id....')
+            except:
+                await message.channel.send('Couldnt find ID, ping adi if this happens too often and he\'ll prolly think of a fix <a:MaidThumbsUp:839411010460975165>')
+
+
 
     await client.process_commands(message)
 
