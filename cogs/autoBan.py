fc4de01a89e770de9a012fb37bcb989d3e905b22
Aditya-Prabhu <52061073+Aditya-Prabhu@users.noreply.github.com>
2021-05-17 15:38:39 +0530

    autoban fix

--- cogs/autoBan.py
+++ cogs/autoBan.py
@@ -19,14 +19,14 @@ class autoBan(commands.Cog):
                     if len(userToBan) == 18:
                         userToBan = await self.bot.fetch_user(userToBan)
                         await message.channel.send(f"""Banning user:{userToBan.name}(#{userToBan.discriminator}) with id:{userToBan.id} for scam reported in the message {message.jump_url}""")
-                        #await message.guild.ban(user= userToBan,reason= 'Ink autoban for being reported by another server. [Link to alert](' + message.jump_url + ')')
+                        await message.guild.ban(user= userToBan,reason= 'Ink autoban for being reported by another server. [Link to alert](' + message.jump_url + ')')
                     else:
                         userToBan = int(message.content[3:21])
                         userToBan = str(userToBan)
                         if len(userToBan) == 18:
                             userToBan = await self.bot.fetch_user(userToBan)
                             await message.channel.send(f"""Banning user:{userToBan.name}(#{userToBan.discriminator}) with id:{userToBan.id} for scam reported in the message {message.jump_url}""")
-                            #await message.guild.ban(user= userToBan,reason= 'Ink autoban for being reported by another server. [Link to alert](' + message.jump_url + ')')
+                            await message.guild.ban(user= userToBan,reason= 'Ink autoban for being reported by another server. [Link to alert](' + message.jump_url + ')')
                         else:
                             await message.channel.send('Couldnt find ID, ping adi if this happens too often and he\'ll prolly think of a fix <a:MaidThumbsUp:839411010460975165>')
                 except ValueError:
