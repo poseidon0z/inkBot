71004474f9e37e6d22d11cd1aa6a6d5ad602be33
Aditya-Prabhu <52061073+Aditya-Prabhu@users.noreply.github.com>
2021-06-21 19:31:10 +0530

    Update messageMania.py

--- cogs/messageMania.py
+++ cogs/messageMania.py
@@ -14,7 +14,6 @@ from discord.ext import commands
 from discord.ext.commands.cooldowns import BucketType
 from discord.ext.commands.core import is_owner
 from json import load
-from discord.ext.commands.errors import CommandOnCooldown, MemberNotFound
 from utils.commandShortenings import is_message_mania_channel
 from utils.botwideFunctions import has_role, is_manager
 import asyncio
@@ -39,8 +38,8 @@ class messageMania(commands.Cog):
 
     @commands.command(name='mute')
     @commands.guild_only()
+    @commands.cooldown(1,120,BucketType.user)
     @commands.check(is_message_mania_channel)
-    @commands.cooldown(1,60,BucketType.user)
     async def mute(self,ctx,target : discord.Member):
         data = cluster[str(ctx.guild.id)]['eventSettings']
         participant_role = data.find_one({'_id' : 'mmParticipantRole'})['role']
@@ -51,23 +50,23 @@ class messageMania(commands.Cog):
                 muterole = ctx.guild.get_role(mute_role)
                 await target.add_roles(muterole)
                 await ctx.send(f'{target.name} has been muted by {ctx.author.name}',delete_after=3)
-                await asyncio.sleep(30)
+                await asyncio.sleep(15)
                 await target.remove_roles(muterole)
             else:
-                await ctx.send(f'{ctx.author.mention} this user cannot be muted by you <:hahahaha:844944845234634762>',delete_after=3)
+                await ctx.reply(f'{ctx.author.mention} this user cannot be muted by you <:hahahaha:844944845234634762>',delete_after=3)
         else:
             await ctx.send(f'Only people with the <@&{participant_role}> role can use this command!',delete_after=3)
 
 
     @commands.command(name='purge')
-    @commands.check(is_message_mania_channel)
     @commands.guild_only()
-    @commands.cooldown(1,300,BucketType.user)
+    @commands.cooldown(1,120,BucketType.user)
+    @commands.check(is_message_mania_channel)
     async def purge(self,ctx):
         data = cluster[str(ctx.guild.id)]['eventSettings']
         participantRole = data.find_one({"_id" : 'mmParticipantRole'})["role"]
         if has_role(participantRole,ctx.author) is True:
-            await ctx.channel.purge(limit=6)
+            await ctx.channel.purge(limit=10)
             await ctx.send(f'{ctx.author.name} has purged 10 messages!',delete_after=3)
         else:
             await ctx.send(f'Only people with the <@&{participantRole}> role can use this command!',delete_after=3)
@@ -75,19 +74,15 @@ class messageMania(commands.Cog):
     
 
     @commands.command(name='kick')
-    @commands.check(is_message_mania_channel)
     @commands.guild_only()
     @commands.cooldown(1,120,BucketType.user)
+    @commands.check(is_message_mania_channel)
     async def kick(self,ctx,target : discord.Member):
         data = cluster[str(ctx.guild.id)]['eventSettings']
         participantRole = data.find_one({"_id" : 'mmParticipantRole'})["role"]
         bypassRole = data.find_one({"_id" : 'mmStaffRole'})["role"]
         if has_role(participantRole,ctx.author) is True:
             if has_role(bypassRole,target) is False:
-                try:
-                    await target.send(f'You have been kicked from {ctx.guild.name} by {ctx.author.name}!\nHere\'s a link to join back:\nhttps://discord.gg/yzY2N3WHbe')
-                except:
-                    pass
                 await ctx.guild.kick(target)
                 await ctx.send(f'{ctx.author.name} kicked {target.name}!',delete_after=3)
             else:
@@ -97,19 +92,20 @@ class messageMania(commands.Cog):
       
 
     @commands.command(name='messagelb')
-    @commands.check_any(commands.has_guild_permissions(administrator=True),is_owner(),is_manager())
     @commands.guild_only()
+    @commands.check_any(commands.has_guild_permissions(administrator=True),is_owner(),is_manager())
     async def messagelb(self,ctx):
         messages = await ctx.channel.history(limit=None).flatten()
         messagedata = {}
-        message_lb_embed = discord.Embed(title='Dank Trades Message Mania Leaderboard!',colour=0xabcdef)
+        message_lb_embed = discord.Embed(title=f'{ctx.guild.name} Leaderboard!',colour=0xabcdef)
         for message in messages:
-            author = message.author.id
-            if author not in messagedata:
-                messagedata[author] = 1
-            else:
-                number_of_messages = messagedata[author] + 1
-                messagedata[author] = number_of_messages
+            if message.author.bot == False:
+                author = message.author.id
+                if author not in messagedata:
+                    messagedata[author] = 1
+                else:
+                    number_of_messages = messagedata[author] + 1
+                    messagedata[author] = number_of_messages
         sorted_tuples = sorted(messagedata.items(), key=lambda item: item[1])
         await ctx.send(f'Gathered data for messages from {len(sorted_tuples)} members')
         i = 1
